242ce978d3136f945823428fe563eb7a8d980c16
Taci Shlosberg <tshlosberg@gmail.com>
2020-08-24 09:31:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(arr, event) {
+
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++ ) {
+    
+        let str = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
+        result.push(str)
+    } 
+    return result;
+}
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0 ) {
+        console.log(countDown--)
+    }
+}
